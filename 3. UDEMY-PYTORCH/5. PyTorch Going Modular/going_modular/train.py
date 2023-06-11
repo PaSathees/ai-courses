48a2b6c8802fe51fbf6e318a1f3f69b2fb93af8f
Sathees Paskaran <73420161+PaSathees@users.noreply.github.com>
2023-06-11 13:16:07 +0000

    Completed PyTorch going modular

---
+++ 3. UDEMY-PYTORCH/5. PyTorch Going Modular/going_modular/train.py
@@ -0,0 +1,67 @@
+"""
+Trains a PyTorch image classification model using device-agnostic code.
+"""
+import os
+import torch
+
+from torchvision import transforms
+from timeit import default_timer as timer 
+
+import data_setup, engine, model_builder, utils
+
+# Setup hyperparameters
+NUM_EPOCHS = 5 
+BATCH_SIZE = 32
+HIDDEN_UNITS = 10
+LEARNING_RATE = 0.001
+
+# Setup directories
+train_dir = "data/pizza_steak_sushi/train"
+test_dir = "data/pizza_steak_sushi/test"
+
+# Setup device agnostic code
+device = "cuda" if torch.cuda.is_available() else "cpu"
+
+# Create transforms
+data_transform = transforms.Compose([
+                                     transforms.Resize((64, 64)),
+                                     transforms.ToTensor()
+])
+
+# Create DataLoader's and get class_names
+train_dataloader, test_dataloader, class_names = data_setup.create_dataloaders(train_dir=train_dir, 
+                                                                               test_dir=test_dir,
+                                                                               transform=data_transform,
+                                                                               batch_size=BATCH_SIZE)
+
+# Create model
+model = model_builder.TinyVGG(input_shape=3,
+                              hidden_units=HIDDEN_UNITS,
+                              output_shape=len(class_names)).to(device)
+
+# Setup loss and optimizer
+loss_fn = torch.nn.CrossEntropyLoss()
+optimizer = torch.optim.Adam(model.parameters(),
+                             lr=LEARNING_RATE)
+
+# Start the timer
+
+start_time = timer()
+
+# Start training with help from engine.py
+engine.train(model=model,
+             train_dataloader=train_dataloader,
+             test_dataloader=test_dataloader,
+             loss_fn=loss_fn,
+             optimizer=optimizer,
+             epochs=NUM_EPOCHS, 
+             device=device)
+
+# End the timer and print out how long it took
+end_time = timer()
+print(f"[INFO] Total training time: {end_time-start_time:.3f} seconds")
+
+# Save the model to file
+utils.save_model(model=model,
+                 target_dir="models",
+                 model_name="05_going_modular_script_mode_tinyvgg_model.pth")
